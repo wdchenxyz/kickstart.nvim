a669d86bd2a38a491d11b73594fef30a8c3b1208
wdchenxyz <wdchenxyz@gmail.com>
2023-05-18 19:18:45 +0800

    ignore files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
 
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers'})
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
@@ -16,11 +16,10 @@ local telescope = require('telescope')
 local custom_options = {
     pickers = {
         find_files = {
-            no_ignore = true
+            no_ignore = false
         }
     }
 }
 
 -- Apply the custom options using the setup function
 telescope.setup(custom_options)
-
