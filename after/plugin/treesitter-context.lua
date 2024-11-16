5966082b5b6b1bb79f764696df5a68b770fe67c5
wdchen <wdchenxyz@gmail.com>
2024-11-16 14:58:58 +0800

    refactor

--- after/plugin/treesitter-context.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "gc", function()
-    require("treesitter-context").go_to_context()
-end, { silent = true })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,8 @@
 return {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    -- dependencies = {
-    --     -- Automatically install LSPs to stdpath for neovim
-    --     { 'williamboman/mason.nvim', config = true },
-    --     {'williamboman/mason-lspconfig.nvim'},
-    --
-    --     -- Useful status updates for LSP
-    --     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    --     { 'j-hui/fidget.nvim',       opts = {} },
-    --
-    --     -- Additional lua configuration, makes nvim stuff amazing!
-    --     'folke/neodev.nvim',
-    -- },
-    {
+
+    dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
@@ -29,51 +18,7 @@ return {
 
     config = function()
 
-        local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
-
-        local on_attach = function(_, bufnr)
-            -- NOTE: Remember that lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself
-            -- many times.
-            --
-            -- In this case, we create a function that lets us more easily define mappings specific
-            -- for LSP related items. It sets the mode, buffer and description for us each time.
-            local nmap = function(keys, func, desc)
-                if desc then
-                    desc = 'LSP: ' .. desc
-                end
-
-                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-            end
-
-            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-            nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>Ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-            -- See `:help K` for why this keymap
-            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-            -- Lesser used LSP functionality
-            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-            nmap('<leader>Wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-            nmap('<leader>Wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-            nmap('<leader>Wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, '[W]orkspace [L]ist Folders')
-
-            -- Create a command `:Format` local to the LSP buffer
-            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                vim.lsp.buf.format()
-            end, { desc = 'Format current buffer with LSP' })
-        end
 
         local capabilities = vim.tbl_deep_extend(
             "force",
@@ -88,15 +33,30 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "gopls",
                 "pylsp"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
-                        -- capabilities = capabilities,
-                        on_attach = on_attach
+                         capabilities = capabilities,
                     }
+                end,
+
+                ['pylsp'] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.pylsp.setup {
+                        settings = {
+                            pylsp = {
+                                plugins = {
+                                    mccabe = { enabled = false },
+                                    pycodestyle = {
+                                        maxLineLength = 120
+                                    }
+                                }
+                            }
+                        }
+                    }
+
                 end,
             }
         })
