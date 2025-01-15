5beaa7bd867e5c7ba097061f83d6ccdcbbab6356
wdchen <wdchen@gmail.com>
2025-01-15 15:57:23 +0800

    feat: remap keys

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,10 +3,10 @@ return {
     keys = {
         { "<leader>a", function() require("harpoon.mark").add_file() end,        desc = "Add file to Harpoon" },
         { "<C-e>",     function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon quick menu" },
-        { "<C-h>",     function() require("harpoon.ui").nav_file(1) end,         desc = "Navigate to Harpoon file 1" },
-        { "<C-t>",     function() require("harpoon.ui").nav_file(2) end,         desc = "Navigate to Harpoon file 2" },
-        { "<C-n>",     function() require("harpoon.ui").nav_file(3) end,         desc = "Navigate to Harpoon file 3" },
-        { "<C-s>",     function() require("harpoon.ui").nav_file(4) end,         desc = "Navigate to Harpoon file 4" },
+        { "<C-t>",     function() require("harpoon.ui").nav_file(1) end,         desc = "Navigate to Harpoon file 1" },
+        { "<C-h>",     function() require("harpoon.ui").nav_file(2) end,         desc = "Navigate to Harpoon file 2" },
+        { "<C-j>",     function() require("harpoon.ui").nav_file(3) end,         desc = "Navigate to Harpoon file 3" },
+        { "<C-n>",     function() require("harpoon.ui").nav_file(4) end,         desc = "Navigate to Harpoon file 4" },
     },
     opts = {}
 }
