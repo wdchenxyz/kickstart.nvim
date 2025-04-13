a529ced4d2499ef19bd36baff81a5c366ed525fa
wdchen <wdchen@gmail.com>
2025-04-13 16:27:57 +0800

    feat: add mcphub and fix some codecompanion problems

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,70 +1,83 @@
 return {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-        { "nvim-lua/plenary.nvim",                     branch = "master" },
-        { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
-        "nvim-treesitter/nvim-treesitter",
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" },
+    -- being setup in individual lua file
+    -- { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+    "nvim-treesitter/nvim-treesitter",
+  },
+  enabled = true,
+  config = true,
+  opts = {
+    display = {
+      chat = {
+        window = {
+          position = "right"
+        },
+        show_settings = false,         -- Show LLM settings at the top of the chat buffer?
+      }
     },
-    enabled = true,
-    config = true,
-    opts = {
-        display = {
-            chat = {
-                window = {
-                    position = "right"
-                }
+    strategies = {
+      chat = {
+        adapter = 'openai',
+        tools = {
+          ["mcp"] = {
+            -- Prevent mcphub from loading before needed
+            callback = function()
+              return require("mcphub.extensions.codecompanion")
+            end,
+            description = "Call tools and resources from the MCP Servers"
+          }
+        },
+        keymaps = {
+          close = {
+            modes = { n = "<C-x>", i = "<C-x>" },
+          }
+        }
+      },
+      inline = { adapter = 'openai' }
+    },
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          env = {
+            api_key = "i have to fill it"
+          },
+          schema = {
+            model = {
+              default = "gemma2:2b",
             }
-        },
-        strategies = {
-            chat = {
-                adapter = 'openai',
-                keymaps = {
-                    close = {
-                        modes = { n = "<C-9>", i = "<C-9>"},
-                    }
-                }
-            },
-            inline = { adapter = 'openai' }
-        },
-        adapters = {
-            ollama = function()
-                return require("codecompanion.adapters").extend("ollama", {
-                    env = {
-                        api_key = "i have to fill it"
-                    },
-                    schema = {
-                        model = {
-                            default = "gemma2:2b",
-                        }
-                    }
-                })
-            end,
-            openai = function()
-                return require("codecompanion.adapters").extend("openai", {
-                    env = {
-                        api_key = "cmd:security find-generic-password -a codecompanion -s OpenAI_codecompanion -w"
-                    },
-                    schema = {
-                        model = {
-                            default = "gpt-4o-mini"
-                        }
-                    }
-                })
-            end,
-            anthropic = function()
-                return require("codecompanion.adapters").extend("anthropic", {
-                    env = {
-                        api_key = "cmd:security find-generic-password -a codecompanion -s Claude_codecompanion -w"
-                    }
-                })
-            end,
-        },
-        keys = {
-            vim.cmd([[cab cc CodeCompanion]]),
-            vim.cmd([[cab cct CodeCompanionChat]]),
-            vim.cmd([[cab cca CodeCompanionActions<cr>]]),
-            vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true }),
-            vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true }),
-        },
-    }
+          }
+        })
+      end,
+      openai = function()
+        return require("codecompanion.adapters").extend("openai", {
+          env = {
+            api_key = "cmd:security find-generic-password -a codecompanion -s OpenAI_codecompanion -w"
+          },
+          schema = {
+            model = {
+              default = "gpt-4o-mini"
+            }
+          }
+        })
+      end,
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = "cmd:security find-generic-password -a codecompanion -s Claude_codecompanion -w"
+          }
+        })
+      end,
+    },
+    keys = {
+      vim.cmd([[cab cc CodeCompanion]]),
+      vim.cmd([[cab cct CodeCompanionChat]]),
+      vim.cmd([[cab cca CodeCompanionActions<cr>]]),
+      vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true }),
+      vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true }),
+      vim.keymap.set("v", "<leader>d", function() require("codecompanion").prompt("explain") end,
+        { noremap = true, silent = true })
+    },
+  }
 }
---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return
+{
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+        file_types = { "markdown", "Avante", "codecompanion" },
+    },
+    ft = { "markdown", "Avante", "codecompanion" },
+}
