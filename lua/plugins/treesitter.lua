72058c4ebc00d7f501d45c033f87650c523ca3b6
wdchen <wdchenxyz@gmail.com>
2025-05-05 23:11:26 +0800

    chore: dummuy

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -73,12 +73,12 @@ return {
         },
     },
     {
-        'nvim-treesitter/nvim-treesitter-context',
-        config = function()
-            vim.keymap.set("n", "gc", function()
-                require("treesitter-context").go_to_context()
-            end, { silent = true })
-        end
+      'nvim-treesitter/nvim-treesitter-context',
+      config = function()
+          vim.keymap.set("n", "gc", function()
+              require("treesitter-context").go_to_context()
+          end, { silent = true })
+      end
 
     }
 }
